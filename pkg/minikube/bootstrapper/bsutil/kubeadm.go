3cfc6492bf1512287f3618ebf96c4b25da4fe6f9
Nir Soffer <nirsof@gmail.com>
2025-09-21 17:27:55 +0300

    Add /usr/sbin to kubeadm path
    It is required to find losetup since
    https://github.com/kubernetes/kubernetes/pull/129450
    
    This is likely a kubernetes bugs since this breaks kubeadm with Debian
    12, but this allows us to switch to Debian with current kubeadm.

--- pkg/minikube/bootstrapper/bsutil/kubeadm.go
+++ pkg/minikube/bootstrapper/bsutil/kubeadm.go
@@ -20,7 +20,10 @@ package bsutil
 import (
 	"bytes"
 	"fmt"
+	"os"
 	"path"
+	"slices"
+	"strings"
 
 	"github.com/blang/semver/v4"
 	"github.com/pkg/errors"
@@ -222,8 +225,17 @@ var KubeadmExtraConfigOpts = []string{
 
 // InvokeKubeadm returns the invocation command for Kubeadm
 func InvokeKubeadm(version string) string {
-	// PATH=/var/lib/minikube/binaries/v1.34.0:$PATH
-	return fmt.Sprintf("sudo env PATH=\"%s:$PATH\" kubeadm", BinRoot(version))
+	// /var/lib/minikube/binaries/v1.34.0
+	prefix := []string{BinRoot(version)}
+
+	// kuebadm try to find losetup in the path since
+	// https://github.com/kubernetes/kubernetes/pull/129450
+	paths := strings.Split(os.Getenv("PATH"), ":")
+	if !slices.Contains(paths, "/usr/sbin") {
+		prefix = append(prefix, "/usr/sbin")
+	}
+
+	return fmt.Sprintf("sudo env PATH=\"%s:$PATH\" kubeadm", strings.Join(prefix, ":"))
 }
 
 // EtcdDataDir is where etcd data is stored.
